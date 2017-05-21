1ace4ba49a7395715696a21b9e1da060d2a515fe
JF-Lalonde <lalondej88@gmail.com>
2017-05-21 15:25:13 -0600

    prints working map

--- lib/battleship_map.rb
+++ lib/battleship_map.rb
@@ -6,14 +6,28 @@
 require './lib/grid_square'
 class BattleshipMap
 
-  attr_reader :size, :grid
+  attr_reader :size, :grid, :x_axis
 
   def initialize(size = 4)
     @size = size
-    @grid = Array.new(size).map! do |x|
-      Array.new(size).map! do |y|
+    @grid = Array.new(size).map! do |y|
+      Array.new(size).map! do |x|
         GridSquare.new
       end
+    end
+    show_map
+  end
+
+  def show_map
+    y_axis = ("A".."Z").to_a
+    @x_axis = (1..size).to_a
+    puts "  " + x_axis.join(" ")
+    @grid.each_with_index do |x, i|
+      print y_axis[i] + " "
+      x.each do |square|
+        print square.hitbox_status + " "
+      end
+        print "\n"
     end
   end
 end
