5b013c0d6e5fcbfe80788f654b78c7ca918ae70f
JF-Lalonde <lalondej88@gmail.com>
2017-05-21 09:42:38 -0600

    Passing first map test

--- lib/battleship_map.rb
+++ lib/battleship_map.rb
@@ -1,2 +1,10 @@
+# Starting with a 4x4 map
 class BattleshipMap
+
+  attr_reader :size
+
+  def initialize(size = 4)
+    @size = size
+  end
+
 end
--- test/battleship_map_test.rb
+++ test/battleship_map_test.rb
@@ -6,5 +6,11 @@ require 'minitest/pride'
 require './lib/battleship_map'
 
 class BattleshipMapTest < Minitest::Test
+  def test_that_map_starts_at_correct_size
+    new_map = BattleshipMap.new(4)
+    actual = new_map.size
+    expected = 4
 
+    assert_equal expected, actual
+  end
 end
