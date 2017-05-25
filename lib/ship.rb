ec0da6681a8f16eabd507cc41e61cdfd51f5f5f2
JF-Lalonde <lalondej88@gmail.com>
2017-05-25 06:53:00 -0600

    Making 6 ship class tests pass

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,13 +1,42 @@
+require './lib/game_map'
+
 class Ship
 
-  attr_reader :length
-  
-  SHIP_SIZE = {first: 2, second: 3}
+  attr_reader :ship_length,
+              :type,
+              :row
+
+  attr_accessor :hits
 
   def initialize(type)
       @type = type
-      @length = SHIP_SIZE[type]
       @hits = 0
+    if @type == :first
+      @ship_length = 2
+    else
+      @ship_length = 3
+    end
   end
 
+  def place_ship(game_map, start_coordinate, orientation)
+    @row = start_coordinate[:row]
+    column = start_coordinate[:column]
+    @ship_length.times do
+      if orientation == :vertical
+        game_map.grid[row][column].hitbox = @type
+        @row += 1
+      else
+        game_map.grid[row][column].hitbox = @type
+        column += 1
+      end
+    end
+  end
+
+  def sunk?
+    @ship_length == @hits
+  end
+
+  def hit
+    @hits += 1
+  end
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,7 +8,7 @@ require './lib/ship'
 class ShipTest < Minitest::Test
   def test_types_of_ships
     ship = Ship.new(:first)
-    actual = ship.length
+    actual = ship.ship_length
     expected = 2
 
     assert_equal expected, actual
@@ -16,8 +16,44 @@ class ShipTest < Minitest::Test
 
   def test_other_types_of_ships
     ship = Ship.new(:second)
-    actual = ship.length
+    actual = ship.ship_length
     expected = 3
+
+    assert_equal expected, actual
+  end
+
+  def test_that_placed_ships_expand_coords
+    ship = Ship.new(:second)
+    map = GameMap.new
+    ship.place_ship(map, {row: 1, column: 0}, :vertical)
+    actual = ship.row
+    expected = 4
+
+    assert_equal expected, actual
+  end
+
+  def test_that_ships_start_unsunk
+    ship = Ship.new(:first)
+    actual = ship.sunk?
+    expected = false
+
+    assert_equal expected, actual
+  end
+
+  def test_that_ships_can_take_hits
+    ship = Ship.new(:second)
+    ship.hits = 2
+    actual = ship.hits
+    expected = 2
+
+    assert_equal expected, actual
+  end
+
+  def test_that_ships_can_be_sunk
+    ship = Ship.new(:first)
+    ship.hits = 2
+    actual = ship.sunk?
+    expected = true
 
     assert_equal expected, actual
   end
